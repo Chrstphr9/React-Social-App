3a0dab5731adda10bc14de8cbae2d3e079b97163
somto <somto565@gmail.com>
2023-09-07 10:17:45 +0100

    added an img

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import { Chat, Notifications, Person, Search } from "@mui/icons-material";
+import "./topbar.css";
 
 export default function Topbar() {
   return (
-    <div>Topbar</div>
-  )
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">Somto Social</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchBar">
+          <Search />
+          <input
+            type="text"
+            placeholder="Search For Friend, Post or Video"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <div className="topbarLink">Homepage</div>
+          <div className="topbarLink">Timeline</div>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <img src='/src/assets/person/1.jpeg' alt="" className="topbarImg" />
+      </div>
+    </div>
+  );
 }
