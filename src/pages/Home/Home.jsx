ca80ac57bc1cc539d14358cd3b4b607050708ed3
somto <somto565@gmail.com>
2023-09-25 09:35:24 +0100

    imported sidebar and edited css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Topbar from '../../components/Topbar/Topbar'
+import Sidebar from '../../components/Sidebar/Sidebar'
 
 export default function Home() {
   return (
+    <>
     <Topbar/>
-    
+    <Sidebar />
+    </>
   )
 }
