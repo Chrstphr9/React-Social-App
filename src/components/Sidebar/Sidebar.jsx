c143b5becd9156a88c8158b9066eaa38a79dccdb
somto <somto565@gmail.com>
2023-09-25 16:35:35 +0100

    created friendList Section

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -47,6 +47,14 @@ export default function Sidebar() {
               <span className="sidebarListItemText">Courses</span>
           </li>
         </ul>
+        <button className="sidebarButton">Show More</button>
+        <hr className="sidebarHr"/>
+        <ul className="sidebarFriendList">
+            <li className="sidebarFriend">
+              <img className="sidebarFriendImg" src="/src/assets/person/2.jpeg" alt="" />
+              <span className="sidebarFriendName">Jane Doe</span>
+            </li>
+        </ul>
       </div>
     </div>
   )
