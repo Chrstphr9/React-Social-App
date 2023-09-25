d58f04cbcafe5c1d2e4c005db80fcda87e081fd3
somto <somto565@gmail.com>
2023-09-25 17:09:51 +0100

    share body

--- src/components/Share/Share.jsx
+++ src/components/Share/Share.jsx
@@ -1,9 +1,24 @@
+import { PermMedia } from "@mui/icons-material"
 import "./Share.css"
 
 export default function Share() {
   return (
-    <div>
-        Heyy, it's Share
+    <div className="share">
+        <div className="shareWrapper">
+            <div className="shareTop">
+                <img className="shareProfileImg" src="" alt="" />
+                <input type="What's On Your Mind" className="shareInput" />
+            </div>
+            <hr className="shareHr" />
+            <div className="shareBottom">
+                <div className="shareOptions">
+                    <div className="shareOptipn">
+                        <PermMedia className="shareIcon"/>
+                        <span className="shareOptionText">Photo or Video</span>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
