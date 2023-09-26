81b5ec3c778678e4cbc3153fbd20f3b5e8e04580
somto <somto565@gmail.com>
2023-09-26 19:03:06 +0100

    added icons

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,7 +1,24 @@
+import { MoreVert } from "@mui/icons-material"
 import "./Post.css"
 
 export default function Post() {
   return (
-    <div>Post</div>
+    <div className="post">
+        <div className="postWrapper">
+            <div className="postTop">
+                <div className="postTopLeft">
+                    <img className="postProfileImg" src="/src/assets/person/3.jpeg" alt="" />
+                    <span className="postUsername">Chrstphr</span>
+                    <span className="postDate"> 5 Mins Ago</span>
+
+                </div>
+                <div className="postTopRight">
+                    <MoreVert />
+                </div>
+            </div>
+            <div className="postCenter"></div>
+            <div className="postBottom"></div>
+        </div>
+    </div>
   )
 }
