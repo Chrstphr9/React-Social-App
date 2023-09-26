27f1d84dc9dfd19fcbb0b82c51b7c42e002c5775
somto <somto565@gmail.com>
2023-09-26 13:52:12 +0100

    share done

--- src/components/Share/Share.jsx
+++ src/components/Share/Share.jsx
@@ -1,4 +1,4 @@
-import { PermMedia } from "@mui/icons-material"
+import { EmojiEmotions, Label, PermMedia, Room } from "@mui/icons-material"
 import "./Share.css"
 
 export default function Share() {
@@ -13,10 +13,23 @@ export default function Share() {
             <div className="shareBottom">
                 <div className="shareOptions">
                     <div className="shareOption">
-                        <PermMedia className="shareIcon"/>
+                        <PermMedia htmlColor="tomato" className="shareIcon"/>
                         <span className="shareOptionText">Photo or Video</span>
                     </div>
+                    <div className="shareOption">
+                        <Label htmlColor="blue" className="shareIcon"/>
+                        <span className="shareOptionText">Tag</span>
+                    </div>
+                    <div className="shareOption">
+                        <Room htmlColor="green" className="shareIcon"/>
+                        <span className="shareOptionText">Location</span>
+                    </div>
+                    <div className="shareOption">
+                        <EmojiEmotions htmlColor="gold" className="shareIcon"/>
+                        <span className="shareOptionText">Feelings</span>
+                    </div>  
                 </div>
+                <div className="shareButton">Share</div>
             </div>
         </div>
     </div>
