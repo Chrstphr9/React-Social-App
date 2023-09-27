e24d15a42ea5ec3f1d1c581206bd4441a5254080
somto <somto565@gmail.com>
2023-09-27 15:23:10 +0100

    birthday section

--- src/components/Rightbar/Rightbar.jsx
+++ src/components/Rightbar/Rightbar.jsx
@@ -2,6 +2,14 @@ import "./Rightbar.css"
 
 export default function Rightbar() {
   return (
-    <div className="rightbar">Rightbar</div>
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img src="/src/assets/gift.png" alt="" className="birthdayImg" />
+          <span className="birthdayText">
+            <b>Paula Green</b> and <b>3 others friends</b> have birthday today</span>
+        </div>
+      </div>
+    </div>
   )
 }
