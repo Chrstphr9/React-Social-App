5e85287f65eed260a7c7a4b7bbbbb3291231503f
somto <somto565@gmail.com>
2023-09-28 22:11:18 +0100

    work

--- src/components/Rightbar/Rightbar.jsx
+++ src/components/Rightbar/Rightbar.jsx
@@ -9,6 +9,16 @@ export default function Rightbar() {
           <span className="birthdayText">
             <b>Paula Green</b> and <b>3 others friends</b> have birthday today</span>
         </div>
+        <img src="/src/assets/ad.png" alt="" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/src/assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   )
