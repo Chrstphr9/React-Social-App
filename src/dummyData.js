3720e2a325d41697beab17d54e5b586237cdd592
somto <somto565@gmail.com>
2023-10-02 09:43:25 +0100

    witing dummyData

---
+++ src/dummyData.js
@@ -0,0 +1,52 @@
+export const User = [
+    {
+        id: 1,
+        profilePicture: "/src/assets/person/1.jpeg",
+        username: "Safak Kocaoglu"
+    },
+    {
+        id: 2,
+        profilePicture: "/src/assets/person/2.jpeg",
+        username: "Akash Roman"
+    },
+    {
+        id: 3,
+        profilePicture: "/src/assets/person/3.jpeg",
+        username: "Somto Anigbo"
+    },
+    {
+        id: 4,
+        profilePicture: "/src/assets/person/4.jpeg",
+        username: "Chrstphr Wlkings"
+    },
+    {
+        id: 5,
+        profilePicture: "/src/assets/person/5.jpeg",
+        username: "Anigbo Chris"
+    },
+    {
+        id: 6,
+        profilePicture: "/src/assets/person/6.jpeg",
+        username: "Dubem Anigbo"
+    },
+    {
+        id: 7,
+        profilePicture: "/src/assets/person/7.jpeg",
+        username: "Octane Riels"
+    },
+    {
+        id: 8,
+        profilePicture: "/src/assets/person/8.jpeg",
+        username: "Tocey Downs"
+    },
+    {
+        id: 9,
+        profilePicture: "/src/assets/person/9.jpeg",
+        username: "Timmy Turner"
+    },
+    {
+        id: 10,
+        profilePicture: "/src/assets/person/10.jpeg",
+        username: "Andrews Dame"
+    },
+]
