176f1a537d7cada691b354a1664fcf8b4569e88c
somto <somto565@gmail.com>
2023-10-14 07:48:57 +0100

    imported login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Home from './pages/Home/Home'
 import Profile from './components/Profile/Profile'
+import Login from './pages/Home/Login/Login'
 
 export default function App() {
   return (
-    <Profile/>
+    <Login/>
   )
 }
--- src/pages/Home/Login/Login.jsx
+++ src/pages/Home/Login/Login.jsx
@@ -1,7 +1,17 @@
 import "./login.css"
 
 export default function Login() {
-  return (
-    <div>Login</div>
-  )
+    return (
+        <div className="login">
+            <div className="loginWrapper">
+                <div className="loginleft">
+                    <div className="loginLogo">Somto Social</div>
+                    <span className="loginDesc">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint, cumque!</span>
+                </div>
+                <div className="loginRight">
+                    <div className="loginBox"> </div>
+                </div>
+            </div>
+        </div>
+    )
 }
