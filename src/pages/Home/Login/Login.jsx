67ba43682799a5fa07c1d5c0a5efefe3fae03345
somto <somto565@gmail.com>
2023-10-14 07:59:54 +0100

    done with logo

--- src/pages/Home/Login/Login.jsx
+++ src/pages/Home/Login/Login.jsx
@@ -4,12 +4,12 @@ export default function Login() {
     return (
         <div className="login">
             <div className="loginWrapper">
-                <div className="loginleft">
+                <div className="loginLeft">
                     <div className="loginLogo">Somto Social</div>
                     <span className="loginDesc">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint, cumque!</span>
                 </div>
                 <div className="loginRight">
-                    <div className="loginBox"> </div>
+                    <div className="loginBox"></div>
                 </div>
             </div>
         </div>
