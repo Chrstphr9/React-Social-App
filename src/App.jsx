0793b83be029b7fc218e82c3e7c608f88b16fa7e
somto <somto565@gmail.com>
2023-10-15 18:49:04 +0100

    done ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import React from 'react'
-import Home from './pages/Home/Home'
-import Profile from './components/Profile/Profile'
-import Login from './pages/Login/Login'
-import Register from './pages/Register/Register'
+ import Home from './pages/Home/Home'
 
 export default function App() {
   return (
     <>
-    <Login/>
-    <Register />
+    <Home />
     </>
     
   )
