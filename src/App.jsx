bbffc722970551f542239df1b6b87172b7fc3d84
somto <somto565@gmail.com>
2023-11-30 11:09:42 +0100

    working

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
  import Home from './pages/Home/Home'
+ import Login from './pages/login/Login'
+ import Register from './pages/register/Register'
+ import Profile from './components/Profile/Profile'
+
+ import { 
+  BrowserRouter as Router,
+  Routes,
+  Route,
+ } from "react-router-dom"
 
 export default function App() {
   return (
-    <>
-    <Home />
-    </>
+    <Router>
+      <Routes>
+      <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profi\
+        \e" element={<Profile />} />
+      </Routes>
+    </Router>
     
   )
 }
+
--- src/dummyData.js
+++ src/dummyData.js
@@ -1,52 +1,52 @@
 export const Users = [
     {
         id: 1,
-        profilePicture: "/src/assets/person/1.jpeg",
+        profilePicture: "/src/person/1.jpeg",
         username: "Safak Kocaoglu"
     },
     {
         id: 2,
-        profilePicture: "/src/assets/person/2.jpeg",
+        profilePicture: "/src/person/2.jpeg",
         username: "Akash Roman"
     },
     {
         id: 3,
-        profilePicture: "/src/assets/person/3.jpeg",
+        profilePicture: "/src/person/3.jpeg",
         username: "Somto Anigbo"
     },
     {
         id: 4,
-        profilePicture: "/src/assets/person/4.jpeg",
+        profilePicture: "/src/person/4.jpeg",
         username: "Chrstphr Wlkings"
     },
     {
         id: 5,
-        profilePicture: "/src/assets/person/5.jpeg",
+        profilePicture: "/src/person/5.jpeg",
         username: "Anigbo Chris"
     },
     {
         id: 6,
-        profilePicture: "/src/assets/person/6.jpeg",
+        profilePicture: "/src/person/6.jpeg",
         username: "Dubem Anigbo"
     },
     {
         id: 7,
-        profilePicture: "/src/assets/person/7.jpeg",
+        profilePicture: "/src/person/7.jpeg",
         username: "Octane Riels"
     },
     {
         id: 8,
-        profilePicture: "/src/assets/person/8.jpeg",
+        profilePicture: "/src/person/8.jpeg",
         username: "Tocey Downs"
     },
     {
         id: 9,
-        profilePicture: "/src/assets/person/9.jpeg",
+        profilePicture: "/src/person/9.jpeg",
         username: "Timmy Turner"
     },
     {
         id: 10,
-        profilePicture: "/src/assets/person/10.jpeg",
+        profilePicture: "/src/person/10.jpeg",
         username: "Andrews Dame"
     },
 ];
@@ -55,7 +55,7 @@ export const Posts = [
     {
         id: 1,
         desc: "Love For All, Hatred For None",
-        photo: "/src/assets/post/1.jpeg",
+        photo: "/src/post/1.jpeg",
         date: "5 Mins Ago",
         userId: 1,
         like: 32,
@@ -64,7 +64,7 @@ export const Posts = [
     {
         id: 2,
         desc:"The only thing that makes sense is to be happy.",
-        photo: "/src/assets/post/2.jpeg",
+        photo: "/src/post/2.jpeg",
         date: "15 Mins Ago",
         userId: 2,
         like: 2,
@@ -73,7 +73,7 @@ export const Posts = [
     {
         id: 3,
         desc: "I am the one who has to make sure you don't get hurt in your sleep!",
-        photo: "/src/assets/post/3.jpeg",
+        photo: "/src/post/3.jpeg",
         date: "35 Mins Ago",
         userId: 3,
         like: 23,
@@ -81,7 +81,7 @@ export const Posts = [
     },
     {
         id: 4,
-        photo: "/src/assets/post/4.jpeg",
+        photo: "/src/post/4.jpeg",
         date: "45 Mins Ago",
         userId: 4,
         like: 22,
